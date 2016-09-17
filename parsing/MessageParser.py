bf4cb0a659a035260777cb0fa72931958209be90
Ahmed El Melegy (GP) <a-ahelme@microsoft.com>
2016-09-17 17:02:07 +0200

    fix missing from and to

--- parsing/MessageParser.py
+++ parsing/MessageParser.py
@@ -18,18 +18,35 @@ class MessageParser():
         resp = resp['entities']['datetime'][0]
         # resp['timezone']='UTC+01'
         if resp['type'] == 'interval':
-            return parser.parse(resp['from']['value']) ,parser.parse(resp['to']['value'])
+            if "from" in resp and "to" not in resp:
+                datefrom = parser.parse(resp['from']['value'])
+                return datefrom ,self.infer_missingtime(datefrom,resp["from"]["grain"],1),isnegative
+            elif "to" in resp and "from" not in resp:
+                dateto = parser.parse(resp['to']['value'])
+
+                return self.infer_missingtime(dateto,resp["to"]["grain"],-1), dateto,isnegative
+            else:
+                datefrom = parser.parse(resp['from']['value'])
+                dateto = parser.parse(resp['to']['value'])
+                return datefrom,dateto,isnegative
+
         else:
             datefrom = parser.parse(resp['value'])
-            if resp['grain'] == 'hour':
-                dateto = datefrom +timedelta(hours=1)
-            if resp['grain'] == 'day':
-                dateto = datefrom +timedelta(days=1)
-            if resp['grain'] == 'week':
-                dateto = datefrom +timedelta(days=7)
-            if resp['grain'] == 'month':
-                dateto = datefrom +timedelta(days=30)
+            dateto = self.infer_missingtime(datefrom,resp['grain'],1)
             return datefrom,dateto,isnegative
+
+    def infer_missingtime(self,datetime,grain,d):
+        if grain == 'hour':
+            dateto = datetime +timedelta(hours=2*d)
+        elif grain == 'day':
+            dateto = datetime +timedelta(days=1*d)
+        elif grain == 'week':
+            dateto = datetime +timedelta(days=2*d)
+        elif grain == 'month':
+            dateto = datetime +timedelta(days=7*d)
+        else:
+            dateto = datetime
+        return dateto
 
     def extract_intent(self,message):
         resp = self.client.message(message)
@@ -44,6 +61,6 @@ class MessageParser():
             return "None"
 
 #m = MessageParser()
-#print m.extract_intent('i won\'t be able to come on Friday.')
+#print m.extract_datetime_range('I am free after 5pm')
 
 
